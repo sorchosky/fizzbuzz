17c215a876fc887c191ed101bf8998df03980e6e
Steve Orchosky <steve@steve.local>
2017-09-15 10:16:20 -0400

    now with working javascript

--- build/js/main.js
+++ build/js/main.js
@@ -1,16 +1,16 @@
-//Variables
-var myHeading = document.getElementById('piToNth__display');
+//fizzbuzz app
+var i = 1;
+var fizzbuzz = "";
 
-//Pi to the Nth digit
-function piToTheNth(){
-  //this variable is the contents of the text field
-  var number = document.getElementById('nth').value;
-  //Take pi and round to the decimal place of "number" (less than or equal to 20)
-  var piToNth = Math.PI.toFixed(number);
-  //Write the full pi value to HTML
-  myHeading.innerHTML = piToNth;
-};
-
-// Console log to test connection
-
-console.log("complete");
+for (i; i < 100; i++) {
+	if (i % 3 == 0 && i % 5 == 0){
+		fizzbuzz = "fizzbuzz";
+	} else if (i % 3 == 0){
+		fizzbuzz = "fizz";
+	} else if (i % 5 == 0){
+		fizzbuzz = "buzz";
+	} else {
+		fizzbuzz = i;
+	};
+	document.getElementById("fizzy").append(fizzbuzz + " ");
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 //fizzbuzz app
+var i = 1;
+var fizzbuzz = "";
+
+for (i; i < 100; i++) {
+	if (i % 3 == 0 && i % 5 == 0){
+		fizzbuzz = "fizzbuzz";
+	} else if (i % 3 == 0){
+		fizzbuzz = "fizz";
+	} else if (i % 5 == 0){
+		fizzbuzz = "buzz";
+	} else {
+		fizzbuzz = i;
+	};
+	document.getElementById("fizzy").append(fizzbuzz + " ");
+}
